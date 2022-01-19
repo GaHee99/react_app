ad7e7f1287be3d17f651b2d12be57388a0e8f89a
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2022-01-19 15:32:28 +0900

    코인 트래커 _ 챌린지

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import {useState} from "react"
-import { useEffect } from "react/cjs/react.development"
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true)
-  //여기서 coins의 초깃값을 안넣어주면, 초깃값이 undefined라서 오류뜬다. 
-  //꼭 빈 배열을 넣어주기
-  const [coins, setCoins] = useState([]) 
-  useEffect( () => {
+  const [coins, setCoins] = useState([])
+  const [coinValue, setCoinValue] = useState(0)
+  const [money, setMoney] = useState(0)
+
+  useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response)=>response.json())
-    .then((json)=> {
-      setCoins(json)
-      setLoading(false)
-    })
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json)
+        setLoading(false)
+      })
   }, [])
+
+  const onChange = (e) => {
+    setMoney(e.target.value)
+  }
+
+  const coinChange = (e) => {
+    setCoinValue(e.target.value)
+  }
+
   return (
     <div>
-      <h1>The Coins ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) =>(
-        <li key={coin.id}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}</li>
+      <h1>Coin Changer {loading ? "" : `(${coins.length})`}</h1>
+
+      {loading ? <strong>Loading...</strong> : 
+      <select onChange={coinChange}>
+        <option>----- Select Coin -----</option>
+        {coins.map((coin, index) => (
+          <option key={index} value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </option>
         ))}
-      </ul>
+      </select>
+      }
+      <hr />
+
+      <div>
+        <input type="number" placeholder="How much you have?" onChange={onChange}/>
+      </div>
+
+      <h1>
+        {(money !== 0 && coinValue !== 0) ? (money / coinValue) : null}
+      </h1>
+
     </div>
   )
-  } 
+}
 
-export default App
+export default App;
