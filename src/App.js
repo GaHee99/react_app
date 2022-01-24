68c8fcd0ce7cad2520b9b26bef701ca62083c1c3
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2022-01-24 19:54:19 +0900

    movie link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Detail from './routes/Detail'
 import Home from './routes/Home'
+import More from './routes/More'
 
 function App() {
   return (
@@ -9,8 +10,14 @@ function App() {
         <Route path="/hello">
           <h1>Hello</h1>
         </Route>
-        <Route path="/movie">
+        <Route path="/movie/:id" //단순히 id라고 쓰면 /movie/id로 보내버린다 
+                                // :필수 
+                                //이렇게되면 Movie컴포넌트에 id가 필요하게 된다
+        >
           <Detail />
+        </Route>
+        <Route path="/movie/more">
+          <h1>More</h1>
         </Route>
         <Route path="/">
           <Home />
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,37 @@
+import {useParams} from "react-router-dom";
+import {useEffect,useState } from "react";
+import { Link } from 'react-router-dom'
 function Detail() {
-  return <h1>Detail</h1>
-}
+  const[loading, setLoading]=useState(true)
+  const[json, setJson]=useState([])
+  const {id} =useParams();
+  
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`) 
+      ).json();
+      setLoading(false);
+      setJson(json.data.movie);
+    }
+  useEffect(()=>{
+    getMovie();
+  },[]);
+  return(
+  <div>
+    {loading ? (<h1>Loading...</h1>) : (
+      <div>
+        <h1>
+        <Link to ={`/movie/${id}/more`}>
+          {json.title}</Link></h1>
+        <img src={json.background_image} />
+        <h3>Intro : {json.description_intro}</h3>
+        <p>Description : {json.description_full}</p>
+        <p>Rating : {json.rating}</p>
+       
+    </div>)}
+    
+  </div>
+
+  )}
+
 export default Detail
